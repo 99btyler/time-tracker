2ee102b2d55767c97946d7406bd162f518fab0ca
Tyler Beringer <99tylerberinger@gmail.com>
2021-04-16 20:59:28 -0400

    Added components TimeTracker, Header, Footer

---
+++ time-tracker/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+function Footer() {
+
+    return (
+        
+        <p>success is not the result of doing an extraordinary thing once, but rather the result of doing unremarkable things many times over a long period of time</p>
+
+    )
+
+}
+
+export default Footer
--- time-tracker/src/index.js
+++ time-tracker/src/index.js
@@ -1,7 +1,9 @@
 import React from "react"
 import ReactDOM from "react-dom"
 
+import TimeTracker from "./TimeTracker"
+
 ReactDOM.render(
-    <h1>Hello, world!</h1>,
+    <TimeTracker />,
     document.getElementById("root")
 );
