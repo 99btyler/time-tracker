0c47dcad6e79a9f923a468055946f39a86b11cfb
Tyler Beringer <99tylerberinger@gmail.com>
2021-04-18 19:14:15 -0400

    Added InputRow.css

--- time-tracker/src/component/components/InputRow.js
+++ time-tracker/src/component/components/InputRow.js
@@ -1,14 +1,16 @@
 import React from "react"
 
+import "./InputRow.css"
+
 function InputRow() {
 
     return (
 
         <div>
-            <input type="text" placeholder="description"/>
-            <input type="text" placeholder="start_time"/>
-            <input type="text" placeholder="end_time"/>
-            <input type="text" value="TOTAL_TIME" readonly />
+            <input type="text" placeholder="description" id="input_description" />
+            <input type="text" placeholder="start_time" id="input_start_time" />
+            <input type="text" placeholder="end_time" id="input_end_time" />
+            <input type="text" value="total_time" readonly id="input_total_time" />
         </div>
 
     )
