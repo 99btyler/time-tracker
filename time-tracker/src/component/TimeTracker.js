2f6637bd8f747c0fbcf61413b33e956226a65dd1
Tyler Beringer <99tylerberinger@gmail.com>
2021-04-20 13:58:10 -0400

    Made amount of rendered InputRow components changeable

--- time-tracker/src/component/TimeTracker.js
+++ time-tracker/src/component/TimeTracker.js
@@ -7,13 +7,20 @@ import InputRow from "./components/InputRow"
 
 function TimeTracker() {
 
+    const amountOfInputRows = 1
+
+    const inputRows = [];
+    for (var i = 0; i < amountOfInputRows; i++) {
+        inputRows.push(<InputRow key={i} />)
+    }
+
     return (
 
         <div>
 
             <Header />
 
-            <InputRow />
+            {inputRows}
             <AddButton />
 
             <Footer />
