18280538023bba54f39bfdc9ff4a0f9ab8b49259
Tyler Beringer <99tylerberinger@gmail.com>
2021-04-24 14:25:57 -0400

    Set autoComplete to off

--- time-tracker/src/component/components/InputRow.js
+++ time-tracker/src/component/components/InputRow.js
@@ -7,7 +7,7 @@ class InputRow extends React.Component {
     render() {
         return (
 
-            <form>
+            <form autoComplete="off">
                 <input id="input-description" type="text" placeholder="description" />
                 <input id="input-timePeriod" type="text" placeholder="timePeriod" />
                 <input id="input-totalTime" type="text" value={this.props.totalTime} readOnly />
