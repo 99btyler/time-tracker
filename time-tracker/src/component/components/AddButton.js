446889ddc9e3e926208dbbb2b6503eb5ea6433d7
Tyler Beringer <99tylerberinger@gmail.com>
2021-06-07 20:55:33 -0400

    Added PropTypes

--- time-tracker/src/component/components/AddButton.js
+++ time-tracker/src/component/components/AddButton.js
@@ -1,11 +1,17 @@
+import PropTypes from "prop-types"
+
 import "./style/AddButton.css"
 
-const AddButton = (props) => {
+const AddButton = ({ handleAddButtonClick }) => {
     return (
 
-        <button id="button-add" onClick={props.handleAddButtonClick}>+</button>
+        <button id="button-add" onClick={handleAddButtonClick}>+</button>
 
     )
+}
+
+AddButton.propTypes = {
+    handleAddButtonClick: PropTypes.func.isRequired
 }
 
 export default AddButton
--- time-tracker/src/component/components/InputRow.js
+++ time-tracker/src/component/components/InputRow.js
@@ -1,15 +1,23 @@
+import PropTypes from "prop-types"
+
 import "./style/InputRow.css"
 
-const InputRow = (props) => {
+const InputRow = ({ inputRowID, handleTimePeriodChange, totalTime }) => {
     return (
 
         <form autoComplete="off">
             <input id="input-description" type="text" placeholder="description" />
-            <input id="input-timePeriod" type="text" placeholder="timePeriod" onChange={(event)=>props.handleTimePeriodChange(event, props.inputRowID)} />
-            <input id="input-totalTime" type="text" value={props.totalTime} readOnly />
+            <input id="input-timePeriod" type="text" placeholder="timePeriod" onChange={(event) => handleTimePeriodChange(event, inputRowID)} />
+            <input id="input-totalTime" type="text" value={totalTime} readOnly />
         </form>
 
     )
+}
+
+InputRow.propTypes = {
+    inputRowID: PropTypes.number.isRequired,
+    handleTimePeriodChange: PropTypes.func.isRequired,
+    totalTime: PropTypes.string.isRequired
 }
 
 export default InputRow
