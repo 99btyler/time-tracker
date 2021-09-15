1d7996b729330ca2038b07676e5843e1b51eca53
Tyler Beringer <99tylerberinger@gmail.com>
2021-09-14 21:26:51 -0400

    Typo

--- time-tracker/backend/server/server.js
+++ time-tracker/backend/server/server.js
@@ -14,7 +14,7 @@ const usersRouter = require("./routes/UsersRouter.js")
 app.use("/users", usersRouter)
 
 // Server
-app.listen(5000, () => console.log(`Server is listening on port ${5000}!`))
+app.listen(5000, () => console.log("Server is listening on port 5000!"))
 
 // Backend
 mongoose.connect(process.env.CONNECTION_STRING, { useNewUrlParser: true, useUnifiedTopology: true }, () => console.log("Database connection established!"))
