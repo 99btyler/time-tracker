caea9379f71399f6e23524e0867db0de49613bc7
99btyler <99tylerberinger@gmail.com>
2023-01-03 13:21:03 -0500

    Stopped using controllers

--- src/backend/routes/entriesRoutes.js
+++ src/backend/routes/entriesRoutes.js
@@ -1,9 +1,30 @@
 const router = require("express").Router();
-const { getEntries, addEntry, editEntry, removeEntry } = require("./controllers/entriesController");
+const EntriesSchema = require("./models/entriesModel");
 
-router.get("/", getEntries);
-router.post("/", addEntry);
-router.put("/:id", editEntry);
-router.delete("/:id", removeEntry);
+router.get("/", (request, response) => {
+    EntriesSchema.find().then(data => response.json(data)).catch(error => response.status(400).json(error));
+});
+
+router.post("/", (request, response) => {
+
+    const NewEntriesSchema = new EntriesSchema(
+        {
+            description: request.body.description,
+            startTime: request.body.startTime,
+            endTime: request.body.endTime
+        }
+    );
+
+    NewEntriesSchema.save().then(newData => response.json(newData)).catch(error => response.status(400).json(error));
+
+});
+
+router.put("/:id", (request, response) => {
+    EntriesSchema.findByIdAndUpdate(request.params.id, request.body).then(() => response.json(`id ${request.params.id} updated`)).catch(error => response.status(400).json(error));
+});
+
+router.delete("/:id", (request, response) => {
+    EntriesSchema.findByIdAndDelete(request.params.id).then(() => response.json(`id ${request.params.id} deleted`)).catch(error => response.status(400).json(error));
+});
 
 module.exports = router;
