28cdf69423ac031e705cf6f2536ab630b7432938
99btyler <99tylerberinger@gmail.com>
2023-01-06 14:00:18 -0500

    Started frontend

---
+++ src/frontend/src/component/TimeTracker.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class TimeTracker extends React.Component {
+
+    render() {
+        return(
+            <div>
+                <h1>time-tracker</h1>
+            </div>
+        )
+    }
+
+}
+
+export default TimeTracker;
---
+++ src/frontend/src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+import TimeTracker from "./component/TimeTracker";
+
+ReactDOM.render(
+  <TimeTracker />,
+  document.getElementById("root")
+);
