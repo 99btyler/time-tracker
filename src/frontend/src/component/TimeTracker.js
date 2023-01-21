0469c87034bf25dc102093d5d186ad0fb2a3bd7a
99btyler <99tylerberinger@gmail.com>
2023-01-21 13:19:12 -0500

    Changed how edits are saved

--- src/frontend/src/component/TimeTracker.js
+++ src/frontend/src/component/TimeTracker.js
@@ -37,29 +37,13 @@ class TimeTracker extends React.Component {
                 <h1>time-tracker</h1>
                 <List data={this.state.data} onChangeDataItem={this.onChangeDataItem} addDataItem={this.addDataItem} removeDataItem={this.removeDataItem} />
                 <br />
-                <button onClick={this.saveDataItems}>SAVE EDITS</button>
             </div>
         )
     }
 
-    onChangeDataItem = (_id, value) => {
-
-       this.setState(previousState => (
-            {
-                data: previousState.data.map(dataItem => {
-                    if (dataItem._id === _id) {
-                        dataItem.description = value;
-                    }
-                    return dataItem;
-                })
-            }
-        ));
-        
-    }
-
     addDataItem = () => {
 
-        axios.post("http://localhost:8080/entries/add", {description: "\0", startTime: "\0", endTime: "\0"}).then(response => {
+        axios.post("http://localhost:8080/entries/add", {description: "description", startTime: "8:00 AM", endTime: "8:33 AM"}).then(response => {
             this.setState(previousState => (
                 {
                     data: [...previousState.data, response.data]
@@ -81,16 +65,24 @@ class TimeTracker extends React.Component {
 
     }
 
-    saveDataItems = () => {
+    onChangeDataItem = (_id, value) => {
 
-        for (let i = 0; i < this.state.data.length; i++) {
+        console.log(`${_id} ====> ${value}`)
 
-            const dataItem = this.state.data[i];
-            
-            axios.put(`http://localhost:8080/entries/edit/${dataItem._id}`, {description: dataItem.description, startTime: dataItem.startTime, endTime: dataItem.endTime}).catch(error => console.log(error));
+        this.setState(previousState => (
+            {
+                data: previousState.data.map(dataItem => {
+                    if (dataItem._id === _id) {
+                        dataItem.description = value;
+                    }
+                    return dataItem;
+                })
+            }
+        ));
 
-        }
-
+        const dataItem = this.state.data.find(dataItem => dataItem._id === _id);
+        axios.put(`http://localhost:8080/entries/edit/${dataItem._id}`, {description: dataItem.description, startTime: dataItem.startTime, endTime: dataItem.endTime}).catch(error => console.log(error));
+         
     }
 
 }
